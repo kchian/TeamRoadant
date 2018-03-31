62722e6b67c54b3ef2f6e5fe7efbb63f59d92b01
JIGAR-XPS\Jigar <jigarhira354@gmail.com>
2018-03-30 21:58:48 -0700

    Added functions for motor speed control Added functions for encoder and ir reading Added PD for front ir emitters

--- arduino/micromouse/micromouse.ino
+++ arduino/micromouse/micromouse.ino
@@ -12,7 +12,13 @@ void turn(float,float,float);
 **/
 
 // Set a motor to a specific speed
-void setMotorSpeed(int, int, int);
+void IR_Calibration();
+void setMotorPower(int, int, int);
+void readEncoders();
+void setRightMotorSpeed(int);
+void setLeftMotorSpeed(int);
+void readIR();
+int PD_IR();
 
 void emit(float);//activate emitters for a given time
 
@@ -47,10 +53,29 @@ const int emit4 = 20;
 // frontRightRecieverer analog pins
 const int receivers[4]={19,18,17,16};
 int reading[4]={0,0,0,0};
-const int frontLeftReciever = 19;
-const int leftReciever = 18;
+const int frontLeftReciever = 18;
+const int leftReciever = 19;
 const int frontRightReciever = 17;
 const int rightReciever = 16;
+
+// IR Reciever variables
+int r1;
+int r2;
+int r3;
+int r4;
+
+// PD IR variables
+int errorP;
+int errorD;
+int oldErrorP;
+int leftMiddleValue;
+int rightMiddleValue;
+int leftWallIR;
+int rightWallIR;
+int rightLeftOffset;
+float totalError;
+float P = 1.0;
+float D = 0.5;
 
 // Encoder variables
 long enc1;
@@ -61,8 +86,8 @@ int enc1_ticksPerCycle;
 int enc2_ticksPerCycle;
 
 // Motor variables
-int m1Speed = 60;
-int m2Speed = 60;
+int m1Speed = 30;
+int m2Speed = 30;
 
 void setup() 
 {
@@ -93,58 +118,22 @@ void setup()
   // Serial Monitor
   Serial.begin(9600);
   Serial.println("Micromouse: Team Roadent");
+
+  // Run calibration
+  delay(5000);
+  IR_Calibration();
 }
 
 void loop()
-{
-  
-  /**
-  digitalWrite(emit1, HIGH);
-  digitalWrite(emit2, HIGH);
-  digitalWrite(emit3, HIGH);
-  digitalWrite(emit4, HIGH);
-  **/
-  
-  int r1;
-  int r2;
-  int r3;
-  int r4;
-  r1 = analogRead(frontRightReciever);
-  r2 = analogRead(frontLeftReciever);
-  r3 = analogRead(rightReciever);
-  r4 = analogRead(leftReciever);
-  
-  // Get Encoder Values
-  enc1 = encoder_m1.read();
-  enc2 = encoder_m2.read();
+{  
+  readIR();
 
-  // Calculate ticksPerCycle
-  enc1_ticksPerCycle = (enc1 - enc1_old);
-  enc2_ticksPerCycle = (enc2 - enc2_old);
+  readEncoders();
 
-  enc1_old = enc1;
-  enc2_old = enc2;
-  
-  setMotorSpeed(m1Forward, m1Reverse, m1Speed);
-  setMotorSpeed(m2Forward, m2Reverse, m2Speed);
+  setRightMotorSpeed(150);
+  setLeftMotorSpeed(150);
 
-  if (enc1_ticksPerCycle < 197)
-  {
-    setMotorSpeed(m1Forward, m1Reverse, m1Speed++);
-  }
-  if (enc1_ticksPerCycle > 203)
-  {
-    setMotorSpeed(m1Forward, m1Reverse, m1Speed--);
-  }
-  
-  if (enc2_ticksPerCycle < 197)
-  {
-    setMotorSpeed(m2Forward, m2Reverse, m2Speed++);
-  }
-  if (enc2_ticksPerCycle > 203)
-  {
-    setMotorSpeed(m2Forward, m2Reverse, m2Speed--);
-  }
+  PD_IR();
 
   Serial.print("Right Reciever: ");
   Serial.println(r3);
@@ -169,12 +158,46 @@ void loop()
   Serial.print("Left Motor Speed: ");
   Serial.println(m1Speed);
   Serial.println();
+  Serial.print("rightLeftOffset: ");
+  Serial.println(rightLeftOffset);
+  Serial.print("errorP: ");
+  Serial.println(errorP);
+  Serial.print("errorD: ");
+  Serial.println(errorD);
+  Serial.print("PD Error: ");
+  Serial.println(totalError);
+  Serial.println();
+  Serial.println();
   
   delay(100);
 
 }
 
-void setMotorSpeed(int mForward, int mReverse, int pwr)
+void IR_Calibration()
+{
+  // Turn emitters on
+  digitalWrite(emit1, HIGH);
+  digitalWrite(emit2, HIGH);
+  digitalWrite(emit3, HIGH);
+  digitalWrite(emit4, HIGH);
+  
+  r1 = analogRead(frontRightReciever);
+  r2 = analogRead(frontLeftReciever);
+
+  // Turn emmitters off
+  digitalWrite(emit1, LOW);
+  digitalWrite(emit2, LOW);
+  digitalWrite(emit3, LOW);
+  digitalWrite(emit4, LOW);
+  
+  leftMiddleValue = r2;
+  rightMiddleValue = r1;
+  leftWallIR = r2 - 5;
+  rightWallIR = r1 - 5;
+  rightLeftOffset = r1 - r2;
+}
+
+void setMotorPower(int mForward, int mReverse, int pwr)
 {
   if (pwr >= 0)
   {
@@ -189,66 +212,105 @@ void setMotorSpeed(int mForward, int mReverse, int pwr)
   }
 }
 
-
-
-
-/**
-void travel (float motorSpeed, float t)//moves  at speed s for time s
+void readEncoders()
 {
-//*** speed  must be between 0 and 255;
-if (motorSpeed>255 || motorSpeed<-255)
-{
-motorSpeed=constrain(motorSpeed,-255,255);
-Serial.println("ERROR: SPEED OUT OF RANGE.");
-}
-motormove(m1Forward,m1Reverse,motorSpeed);
-motormove(m2Forward,m2Reverse,motorSpeed);
-delay(t);
+  // Get Encoder Values
+  enc1 = encoder_m1.read();
+  enc2 = encoder_m2.read();
+
+  // Calculate ticksPerCycle
+  enc1_ticksPerCycle = (enc1 - enc1_old);
+  enc2_ticksPerCycle = (enc2 - enc2_old);
+
+  enc1_old = enc1;
+  enc2_old = enc2;
 }
 
+void setRightMotorSpeed(int targetSpeed)
+{  
+  setMotorPower(m2Forward, m2Reverse, m2Speed);
 
-float Receive()//get reading from receivers
-{
-static int i;
-int result
-for (i=0;i<4;i++)
-{
-analogRead(receivers[i]);
-}
-return
-}
-
-
-void emit(float t)//flash LED's for a given time t
-{
-int i;
-for (i=0;i<4;i++)
-{
-digitalWrite(emitters[i],HIGH);
-delay(t);//stay on for time t in ms 
-digitalWrite(emitters[i],LOW);
-delay(t);
-}
-}
-
-void turn(float rightspeed,float leftspeed, float t)//turns by spinning motors at different speeds.
-{
-motormove(m1Forward,m1Reverse,rightspeed);
-motormove(m2Forward,m2Reverse,leftspeed);
-delay(t);//in milliseconds
-}  
-
-void motormove (int pinforward,int pinbackward ,float velocity)//velocity for a single motor
-{
-if (velocity>=0)
+  // Proportional Adjustment  
+  if (enc2_ticksPerCycle < (targetSpeed - 3))
   {
-  analogWrite(pinforward,velocity);
-  digitalWrite(pinbackward,LOW);
+    setMotorPower(m2Forward, m2Reverse, m2Speed++);
   }
-if (velocity<0)//reverse if motorvelocity is negative
+  if (enc2_ticksPerCycle > (targetSpeed + 3))
   {
-  analogWrite(pinbackward,-velocity);//makes value positive
-  digitalWrite(pinforward,LOW);
+    setMotorPower(m2Forward, m2Reverse, m2Speed--);
   }
 }
-**/
+
+void setLeftMotorSpeed(int targetSpeed)
+{
+  setMotorPower(m1Forward, m1Reverse, m1Speed);
+
+  // Proportional Adjustment
+  if (enc1_ticksPerCycle < (targetSpeed - 3))
+  {
+    setMotorPower(m1Forward, m1Reverse, m1Speed++);
+  }
+  if (enc1_ticksPerCycle > (targetSpeed + 3))
+  {
+    setMotorPower(m1Forward, m1Reverse, m1Speed--);
+  }
+}
+
+void readIR()
+{
+  // Turn emitters on
+  digitalWrite(emit1, HIGH);
+  digitalWrite(emit2, HIGH);
+  digitalWrite(emit3, HIGH);
+  digitalWrite(emit4, HIGH);
+
+  // Get emitter values
+  r1 = analogRead(frontRightReciever);
+  r2 = analogRead(frontLeftReciever);
+  r3 = analogRead(rightReciever);
+  r4 = analogRead(leftReciever);
+
+  // Turn emmitters off
+  digitalWrite(emit1, LOW);
+  digitalWrite(emit2, LOW);
+  digitalWrite(emit3, LOW);
+  digitalWrite(emit4, LOW);
+}
+
+// IR PD for going straight
+int PD_IR()
+{  
+  // If there are both walls
+  if ((r2 > leftWallIR) && (r1 > rightWallIR))
+  {
+    errorP = r1 - r2 - rightLeftOffset;
+    errorD = errorP - oldErrorP;
+  }
+
+  // Only left wall
+  else if (r2 > leftWallIR)
+  {
+    errorP = 2 * (leftMiddleValue - r2);
+    errorD = errorP - oldErrorP;
+  }
+
+  // Only right wall
+  else if (r1 > rightWallIR)
+  {
+    errorP = 2 * (r1 - rightMiddleValue);
+    errorD = errorP - oldErrorP;
+  }
+
+  // If no walls
+  else if ((r2 < leftWallIR) && (r1 < rightWallIR))
+  {
+    errorP = 0;
+    errorD = 0;
+  }
+
+  totalError = (P * (float)errorP) + (D * (float)errorD);
+  oldErrorP = errorP;
+  
+  return totalError;
+}
+
